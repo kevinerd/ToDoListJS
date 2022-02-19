8cda82121ec9158b67a447b8162092c5134fd58f
Kevin Firmani <kevinjf2011@gmail.com>
2022-02-19 14:24:33 -0300

    Versión básica funcionando.

--- app.js
+++ app.js
@@ -1,53 +1,79 @@
-// Referencias de páginas
+// Referencias de páginas.
 let pantallaBienvenida = document.getElementById("pantallaBienvenida");
 let pantallaFormulario = document.getElementById("pantallaFormulario");
 let pantallaListado = document.getElementById("pantallaListado");
 let pantallaDetalle = document.getElementById("pantallaDetalle");
-let formTitulo = document.getElementById("formTitulo");
+
+// Referencias del detalle.
+let imgDetalle = document.getElementById("imgDetalle");
+let tituloDetalle = document.getElementById("tituloDetalle");
+let notasDetalle = document.getElementById("notasDetalle");
+let cerrarDetalle = document.getElementById("cerrarDetalle");
+
+// Elementos del formulario.
+let formItem = document.getElementById("formItem");
 let formCategoria = document.getElementById("formCategoria");
 let formNotas = document.getElementById("formNotas");
+
+// Btn que envía el formulario.
 let formAgregar = document.getElementById("formAgregar");
+
 let listado = document.getElementById("listado");
 
-
+// Btn Bienvenida.
 let agregarItem = document.getElementById("agregarItem");
+
+// Btn Listado.
+let agregarItemListado = document.getElementById("agregarItemListado");
 
 agregarItem.addEventListener( 'click', () => {
     pantallaBienvenida.style.display = "none";
     pantallaFormulario.style.display = "block";
 });
 
+agregarItemListado.addEventListener( 'click', () => {
+    pantallaListado.style.display = "none";
+    pantallaFormulario.style.display = "block";
+} );
+
 let mostrarListado = () => {
     pantallaFormulario.style.display = "none";
     pantallaListado.style.display = "block";
 }
 
-let mostrarDetalle = () => {
+let mostrarDetalle = (item, categoria, notas) => {
     pantallaListado.style.display = "none";
     pantallaDetalle.style.display = "block";
+    imgDetalle.setAttribute("src", `./images/${categoria}`);
+    tituloDetalle.innerHTML = item;
+    notasDetalle.innerHTML = notas;
 }
 
 formAgregar.addEventListener( 'click', () => {
+    let item = formItem.value;
     let categoria = formCategoria.value;
-    let titulo = formTitulo.value;
     let notas = formNotas.value;
     let modelo = `<li class="list-group-item">
-    <img src="images/${categoria}.png" alt="">
-    <p>${titulo}</p>
-    <button onclick="mostrarDetalle()">
-        <img src="images/flecha.png" alt="">
+    <img src="./images/${categoria}" alt="">
+    <p>${item}</p>
+    <button onclick="mostrarDetalle('${item}', '${categoria}', '${notas}')">
+        <img src="./images/flecha.png" alt="">
     </button>
 </li>`;
 
-    if( categoria != "categorias" && titulo != "" && notas != "") {
+    if( categoria != "Categoría..." && item != "" && notas != "") {
         listado.innerHTML += modelo;
         formCategoria.value = "";
-        formTitulo.value = "";
+        formItem.value = "";
         formNotas.value = "";
-        console.log("Item cargado: ", categoria, ' ', titulo, ' ', notas);
         mostrarListado();
     } else {
-        alert("COMPLETAR LOS DATOS!");
+        alert("COMPLETÁ LOS DATOS, BOBO!");
     }
     
 })
+
+cerrarDetalle.addEventListener( 'click', () => {
+    pantallaListado.style.display = "block";
+    pantallaDetalle.style.display = "none";
+} );
